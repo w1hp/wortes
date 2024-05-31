38820f6c140bb1c1067782ca377c971af4848b17
rozyckijan <62714125+rozyckijan@users.noreply.github.com>
2024-05-31 05:09:14 +0200

    Enemies spawning and early AI

---
+++ Assets/Scripts/Enemy/EnemyAISystem.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Transforms;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public partial struct EnemyAISystem : ISystem
+{
+    private EntityManager entityManager;
+    private Entity playerEntity;
+
+    private void OnUpdate(ref SystemState state)
+    {
+        entityManager = state.EntityManager;
+        playerEntity = SystemAPI.GetSingletonEntity<Player>();
+
+        foreach (var (enemyComponent, transformComponent) in SystemAPI.Query<EnemyComponent, RefRW<LocalTransform>>())
+        {
+            float3 direction = entityManager.GetComponentData<LocalTransform>(playerEntity).Position - transformComponent.ValueRO.Position;
+            float angle = math.atan2(direction.z, direction.x);
+            transformComponent.ValueRW.Rotation = quaternion.Euler(new float3(0, angle, 0));
+
+            transformComponent.ValueRW.Position += math.normalize(direction) * SystemAPI.Time.DeltaTime;
+        }
+    }
+}
