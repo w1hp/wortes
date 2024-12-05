4f4f0b536b805771c4f90e344157b386fc2e1c50
golomp <48314220+w1hp@users.noreply.github.com>
2024-12-05 21:06:20 +0100

    finish merge

--- Assets/Scripts/Analytics/AnalyticsManager.cs
+++ Assets/Scripts/Analytics/AnalyticsManager.cs
@@ -2,13 +2,56 @@ using UnityEngine;
 using System.Collections.Generic;
 using Unity.Services.Analytics;
 using Unity.Services.Core;
+using System.Threading.Tasks;
 
 public class AnalyticsManager : MonoBehaviour
 {
-	async void Start()
+	private async void Start()
 	{
-		await UnityServices.InitializeAsync();
-		AnalyticsService.Instance.StartDataCollection();
+		await InitializeUnityServices();
+	}
+	private async Task InitializeUnityServices()
+	{
+		try
+		{
+			await UnityServices.InitializeAsync();
+			if (AnalyticsService.Instance != null)
+			{
+				AnalyticsService.Instance.StartDataCollection();
+				Debug.Log("Unity Analytics initialized and data collection started.");
+			}
+		}
+		catch (System.Exception e)
+		{
+			Debug.LogError($"Failed to initialize Unity Services: {e.Message}");
+		}
+	}
+
+	public void LogLowFrameRateEvent(float frameRate, int levelID, float timeBelowThreshold)
+	{
+		var lowFrameRateEvent = new GameAnalytics.LowFrameRateEvent
+		{
+			FrameRate = frameRate,
+			LevelID = levelID,
+			TimeBelowThreshold = timeBelowThreshold
+		};
+
+		AnalyticsService.Instance.RecordEvent(lowFrameRateEvent);
+		Debug.Log($"Logged LowFrameRateEvent: FrameRate={frameRate}, LevelID={levelID}, TimeBelowThreshold={timeBelowThreshold}");
+	}
+
+	public void LogEnemyDefeatedEvent(int enemyID, int killerID, int levelID)
+	{
+		var enemyDefeatedEvent = new GameAnalytics.EnemyDefeatedEvent(enemyID, killerID, levelID);
+		AnalyticsService.Instance.RecordEvent(enemyDefeatedEvent);
+		Debug.Log($"Logged EnemyDefeatedEvent: EnemyID={enemyID}, KillerID={killerID}, LevelID={levelID}");
+	}
+
+	public void LogUpgradeChoiceEvent(int levelID, int upgradeID)
+	{
+		var upgradeChoiceEvent = new GameAnalytics.UpgradeChoiceEvent(levelID, upgradeID.ToString());
+		AnalyticsService.Instance.RecordEvent(upgradeChoiceEvent);
+		Debug.Log($"Logged UpgradeChoiceEvent: LevelID={levelID}, UpgradeID={upgradeID}");
 	}
 }
 
