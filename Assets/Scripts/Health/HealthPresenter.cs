0b1717d28696f1ce1bc8a1b4d1091bb2f4cfd461
golomp <48314220+w1hp@users.noreply.github.com>
2024-12-06 13:04:32 +0100

    rebalance player shooting

--- Assets/Scripts/Health/HealthPresenter.cs
+++ Assets/Scripts/Health/HealthPresenter.cs
@@ -8,15 +8,20 @@ using UnityEngine.UI;
 public class HealthPresenter : MonoBehaviour
 {
 	[SerializeField] private Slider _healthSlider;
-	[SerializeField] private TextMeshProUGUI _healthText;
+	//[SerializeField] private TextMeshProUGUI _healthText;
 
 	public void UpdateView(int currentHealth, int maxHealth)
 	{
-		if (_healthSlider != null && _healthText != null)
+		//if (_healthSlider != null && _healthText != null)
+		//{
+		//	_healthSlider.maxValue = maxHealth;
+		//	_healthSlider.value = currentHealth;
+		//	_healthText.text = $"HP: {currentHealth}";
+		//}
+		if (_healthSlider != null )
 		{
 			_healthSlider.maxValue = maxHealth;
 			_healthSlider.value = currentHealth;
-			_healthText.text = $"HP: {currentHealth}";
 		}
 	}
 }
