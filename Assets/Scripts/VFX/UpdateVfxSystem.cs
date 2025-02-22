e1d5af07ba7f612c2623cfe88ce4e8ec54327344
golomp <48314220+w1hp@users.noreply.github.com>
2025-02-22 01:04:15 +0100

    add sfx gun

--- Assets/Scripts/VFX/UpdateVfxSystem.cs
+++ Assets/Scripts/VFX/UpdateVfxSystem.cs
@@ -9,35 +9,38 @@ partial struct UpdateVfxSystem : ISystem
 {
 	public void OnUpdate(ref SystemState state)
 	{
-//		foreach (var (localToWorld, vfxComponent, entity) in
-//			SystemAPI.Query<RefRO<LocalToWorld>, RefRW<VfxComponent>>().WithEntityAccess())
-//		{
-////			float3 offset = float3.zero;
+		//		foreach (var (localToWorld, vfxComponent, entity) in
+		//			SystemAPI.Query<RefRO<LocalToWorld>, RefRW<VfxComponent>>().WithEntityAccess())
+		//		{
+		////			float3 offset = float3.zero;
 
-////			VfxPool.Instance.UpdateVfx(
-////				localToWorld.ValueRO.Position + offset,
-////				localToWorld.ValueRO.Rotation,
-////				entity);
+		////			VfxPool.Instance.UpdateVfx(
+		////				localToWorld.ValueRO.Position + offset,
+		////				localToWorld.ValueRO.Rotation,
+		////				entity);
 
-////			if (vfxComponent.ValueRO.VfxType == VfxType.OneShot)
-////			{
-////				vfxComponent.ValueRW.Time -= SystemAPI.Time.DeltaTime;
-//////				if (vfxComponent.ValueRW.Time < 0)
-//////				{
-//////#if UNITY_EDITOR
-//////					UnityEngine.Debug.Log("Bu");
-//////#endif
-//////					VfxPool.Instance.RemoveVfx(entity);
-//////					state.EntityManager.SetComponentEnabled<VfxComponent>(entity, false);
-//////				}
-////			}
-//		}
-		foreach (var (particleSystem, entity) in 
-			SystemAPI.Query<SystemAPI.ManagedAPI.UnityEngineComponent<UnityEngine.ParticleSystem>>()
+		////			if (vfxComponent.ValueRO.VfxType == VfxType.OneShot)
+		////			{
+		////				vfxComponent.ValueRW.Time -= SystemAPI.Time.DeltaTime;
+		//////				if (vfxComponent.ValueRW.Time < 0)
+		//////				{
+		//////#if UNITY_EDITOR
+		//////					UnityEngine.Debug.Log("Bu");
+		//////#endif
+		//////					VfxPool.Instance.RemoveVfx(entity);
+		//////					state.EntityManager.SetComponentEnabled<VfxComponent>(entity, false);
+		//////				}
+		////			}
+		//		}
+		foreach (var (particleSystem, audioSource, entity) in
+			SystemAPI.Query<SystemAPI.ManagedAPI.UnityEngineComponent<UnityEngine.ParticleSystem>,
+				SystemAPI.ManagedAPI.UnityEngineComponent<UnityEngine.AudioSource>>()
 				.WithAll<VfxComponent>()
 				.WithEntityAccess())
 		{
 			particleSystem.Value.Play(true);
+			audioSource.Value.pitch = UnityEngine.Random.Range(0.8f, 1.2f);
+			audioSource.Value.Play();
 			state.EntityManager.SetComponentEnabled<VfxComponent>(entity, false);
 		}
 
